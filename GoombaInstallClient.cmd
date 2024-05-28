2958c2b7bab94c9329c5b72d6a66b6e03ba45828
GoombaStudios <140081099+GoombaStudios@users.noreply.github.com>
2024-05-28 21:17:53 +0200

    Add files via upload

---
+++ GoombaInstallClient.cmd
@@ -0,0 +1,57 @@
+@echo off
+
+:home
+
+echo LOAING...
+md data
+cd data
+
+del list.txt
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/GoombaStudios/battest/main/AppList.txt -Outfile list.txt"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/GoombaStudios/battest/lists/linkcode -Outfile links.cmd"
+call links.cmd
+color 3f
+cls
+echo.
+echo  What do you want to install?
+echo.
+type list.txt
+echo.
+set /p dl=
+
+set dllink=0
+
+if %dl%==1 set dllink=%opt1%
+if %dl%==1 set dlname=%n1%
+
+if %dllink%==0 exit
+
+if exist %dlname% goto al_inst
+
+cd..
+cd..
+
+md %dlname%
+cd %dlname%
+
+powershell -Command "Invoke-WebRequest %dllink% -Outfile %dlname%.cmd"
+
+goto yes
+
+:al_inst
+cls
+color 04
+echo.
+echo  Already installed.
+echo.
+pause
+exit
+
+:yes
+cls
+color 02
+echo.
+echo SUCCESS!
+echo.
+pause
+goto home
