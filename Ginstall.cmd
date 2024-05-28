01d7261ad36755fa67d13a388f2920c90c6e741a
GoombaStudios <140081099+GoombaStudios@users.noreply.github.com>
2024-05-28 21:22:41 +0200

    Add files via upload

---
+++ Ginstall.cmd
@@ -0,0 +1,46 @@
+@echo off
+color 03
+
+set ver=1.0
+
+title Ginstall ver %ver%
+
+cls
+echo.
+echo  Goomba Installer version %ver%                                                                                                    
+echo.                                                                                                    
+echo.                                                                                                    
+echo.                                                                                                    
+echo.                                                                                                    
+echo.                                                   .-.                                              
+echo.                                           .-*@@@@@@@@@@@@%+..                                      
+echo.                                      .@@@@@@@@@@@@@@@@@@@@@@@@:                                    
+echo.                                     -@@@@@@@@@@@@@@@@@@@@@@@@+                                     
+echo.                                    =@@@@@@@@@@@@@@@@@@@@@@@@#.                                     
+echo.                                   =@@@@@@@@@@@@@@+*@@@@@@@@%.                                      
+echo.                                 .*@@@@@@@@@@@@@=. . . .+@@@.                                       
+echo.                                 +@@@@@@@@@@@@+.                                                    
+echo.                                :@@@@@@@@@@@@%. . . . . -= .#%@@:                                 
+echo.                                 #@@@@@@@@@@@% . . @@@@@@@@@@@@@@@-                                 
+echo.                                 .@@@@@@@@@@@# . . %@@@@@@@@@@@@@@+                                 
+echo.                                 .#@@@@@@@@@@#. . . @@@@@@@@@@@@@@#                                 
+echo.                                  :@@@@@@@@@@#. . . @#:....@@@@@@@@@.                                
+echo.                                   +@@@@@@@@@@@=. . . . :#@@@@@@@@@.                                
+echo.                                   :@@@@@@@@@@@@@@@*@@@@@@@@@@@@@@@:                                
+echo.                                   .*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@-                                
+echo.                                    .%@@@@@@@@@@@@@@@@@@@@@@@@@@-.                                 
+echo.                                        :#@@@@@@@@@@@@@@@@@@=.                                      
+echo.                                            .:%@@@@@@@@+.                                           
+echo.                                                  :                                                
+echo.                                                                                                    
+echo.                                                                                                    
+echo.                                                                                                    
+ping github.com -n 2 >nul
+C:
+cd "C:\"
+md GoombaApps
+cd GoombaApps
+powershell -Command "Invoke-WebRequest powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/GoombaStudios/battest/main/GoombaInstallClient.cmd -Outfile links.cmd" -Outfile GappStore.cmd"
+color 2f
+echo Done!
+ping github.com -n 2 >nul                      
